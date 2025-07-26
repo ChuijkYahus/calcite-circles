1da8875afceaa63fac215c9ce99cb7fd0a415ba8
miyucomics <nbriannyc@gmail.com>
2025-07-25 21:25:27 -0400

    Chalk block and item

--- src/main/java/miyucomics/calcite/CalciteClient.kt
+++ src/main/java/miyucomics/calcite/CalciteClient.kt
@@ -1,12 +1,17 @@
 package miyucomics.calcite
 
+import at.petrak.hexcasting.api.casting.iota.IotaType
+import at.petrak.hexcasting.api.client.ScryingLensOverlayRegistry
+import miyucomics.calcite.chalk.ChalkBlockEntity
+import miyucomics.calcite.chalk.ChalkBlockEntityRenderer
 import net.fabricmc.api.ClientModInitializer
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
-import net.minecraft.particle.DefaultParticleType
-import net.minecraft.registry.Registries
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactories
 
 class CalciteClient : ClientModInitializer {
 	override fun onInitializeClient() {
-
+		BlockEntityRendererFactories.register(CalciteMain.CHALK_BLOCK_ENTITY) { ChalkBlockEntityRenderer() }
+		ScryingLensOverlayRegistry.addDisplayer(CalciteMain.CHALK_BLOCK) { lines, _, pos, _, world, _ ->
+			IotaType.getDisplay((world.getBlockEntity(pos) as ChalkBlockEntity).iota)
+		}
 	}
 }
--- src/main/java/miyucomics/calcite/CalciteMain.kt
+++ src/main/java/miyucomics/calcite/CalciteMain.kt
@@ -1,14 +1,24 @@
 package miyucomics.calcite
 
+import miyucomics.calcite.chalk.ChalkBlock
+import miyucomics.calcite.chalk.ChalkBlockEntity
+import miyucomics.calcite.chalk.ChalkItem
 import net.fabricmc.api.ModInitializer
+import net.minecraft.block.entity.BlockEntityType
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
 import net.minecraft.util.Identifier
 
 class CalciteMain : ModInitializer {
 	override fun onInitialize() {
-
+		Registry.register(Registries.ITEM, id("chalk"), ChalkItem())
+		Registry.register(Registries.BLOCK, id("chalk"), CHALK_BLOCK)
+		Registry.register(Registries.BLOCK_ENTITY_TYPE, id("chalk"), CHALK_BLOCK_ENTITY)
 	}
 
 	companion object {
 		fun id(string: String) = Identifier("calcite", string)
+		val CHALK_BLOCK: ChalkBlock = ChalkBlock()
+		val CHALK_BLOCK_ENTITY: BlockEntityType<ChalkBlockEntity> = BlockEntityType.Builder.create(::ChalkBlockEntity, CHALK_BLOCK).build(null)
 	}
 }
