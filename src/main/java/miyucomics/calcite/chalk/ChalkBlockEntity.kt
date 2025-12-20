641f72d50fc58c661699acae9a806185c8f20f3c
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-12-20 14:28:54 -0500

    Chalk pixel rasterization

--- src/main/java/miyucomics/calcite/chalk/ChalkBlockEntity.kt
+++ src/main/java/miyucomics/calcite/chalk/ChalkBlockEntity.kt
@@ -1,6 +1,10 @@
 package miyucomics.calcite.chalk
 
+import at.petrak.hexcasting.api.casting.math.HexDir
+import at.petrak.hexcasting.api.casting.math.HexPattern
 import miyucomics.calcite.CalciteMain
+import miyucomics.calcite.utils.PixelPos
+import miyucomics.calcite.utils.PixelRasterizer
 import net.minecraft.block.BlockState
 import net.minecraft.block.entity.BlockEntity
 import net.minecraft.nbt.NbtCompound
@@ -9,6 +13,7 @@ import net.minecraft.util.math.BlockPos
 
 class ChalkBlockEntity(pos: BlockPos, state: BlockState) : BlockEntity(CalciteMain.CHALK_BLOCK_ENTITY, pos, state) {
 	var iota: NbtCompound = NbtCompound()
+	var pixelCache: Set<PixelPos> = setOf()
 
 	override fun writeNbt(compound: NbtCompound) {
 		compound.put("iota", iota)
@@ -16,6 +21,12 @@ class ChalkBlockEntity(pos: BlockPos, state: BlockState) : BlockEntity(CalciteMa
 
 	override fun readNbt(compound: NbtCompound) {
 		iota = compound.getCompound("iota")
+
+		val pattern = if (iota.getString("hexcasting:type") == "hexcasting:pattern") {
+			HexPattern.fromNBT(iota.getCompound("hexcasting:data"))
+		} else
+			HexPattern.fromAngles("aa", HexDir.EAST)
+		pixelCache = PixelRasterizer.getLinePixels(PixelRasterizer.getNormalizedStrokes(pattern), 16)
 	}
 
 	override fun toInitialChunkDataNbt(): NbtCompound = createNbt()
